f1825a8423472514cbc3684e817e340739da97c4
Emi <emraan.jadidi@outlook.com>
2019-12-06 21:41:29 +0330

    Add ProductFeature Migrations

---
+++ DataLayer/Entities/Product/ProductFeature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    /// <summary>
+    /// ویژگی محصولات
+    /// </summary>
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+
+        public int FeatureId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string FeatureValue { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey(nameof(FeatureId))]
+        public virtual Feature Feature { get; set; }
+    }
+}
---
+++ Service/Migrations/20191206181049_ProductFeatureMigrations.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Migrations
+{
+    public partial class ProductFeatureMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductFeature",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FeatureId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    FeatureValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductFeature", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductFeature_Feature_FeatureId",
+                        column: x => x.FeatureId,
+                        principalTable: "Feature",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductFeature_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductFeature_FeatureId",
+                table: "ProductFeature",
+                column: "FeatureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductFeature_ProductId",
+                table: "ProductFeature",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductFeature");
+        }
+    }
+}
