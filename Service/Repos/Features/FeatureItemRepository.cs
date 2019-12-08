70ebdcc848e9baf0cc10f63df4fe3bb225bd4146
Emi <emraan.jadidi@outlook.com>
2019-12-08 11:59:48 +0330

    Develop OF FeatureItems

---
+++ Service/Repos/Features/FeatureItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Repos
+{
+    public class FeatureItemRepository : GenericRepository<FeatureItem>
+    {
+        public FeatureItemRepository(DatabaseContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
