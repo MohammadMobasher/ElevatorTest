1d0cccc876f0790ae36b70194573ab8e297bfafc
Emraanjadidi <emraan.jadidi@outlook.com>
2020-01-13 22:45:33 +0330

    WareHouse

--- Service/Repos/Warehouses/WarehouseProductCheckRepository.cs
+++ Service/Repos/Warehouses/WarehouseProductCheckRepository.cs
@@ -1,6 +1,10 @@
-﻿using DataLayer.Entities.Warehouse;
+﻿using AutoMapper.QueryableExtensions;
+using DataLayer.DTO.WarehouseProductCheckDTO;
+using DataLayer.Entities.Warehouse;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +16,16 @@ namespace Service.Repos.Warehouses
         {
         }
 
-        ///// <summary>
-        ///// گرفتن تمامی تراکنش های این انبار بر اساس شناس هانبار
-        ///// </summary>
-        ///// <param name="warehouse"></param>
-        ///// <returns></returns>
-        //public async Task<WarehouseProductCheck> GetAllTransactionInWarehouse(int warehouse)
-        //    => await TableNoTracking.Whe
-            
-        
+        /// <summary>
+        /// گرفتن تمامی تراکنش های این انبار بر اساس شناس هانبار
+        /// </summary>
+        /// <param name="warehouse"></param>
+        /// <returns></returns>
+        public async Task<List<WarehouseProductCheckFullDTO>> GetAllTransactionInWarehouse(int warehouse)
+            => await TableNoTracking.Where(a => a.WarehouseId == warehouse)
+            .ProjectTo<WarehouseProductCheckFullDTO>().ToListAsync();
+
+
+
     }
 }
