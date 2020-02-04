7e37da2f3bf45cf4fd5b5a27f99de3319e0d7bd8
Emraanjadidi <emraan.jadidi@outlook.com>
2020-02-04 23:55:41 +0330

    Develop Of Ui

--- Elevator/Controllers/Product/ProductController.cs
+++ Elevator/Controllers/Product/ProductController.cs
@@ -8,16 +8,20 @@ using Elevator.Models;
 using Service.Repos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-
+using Service.Repos.Product;
+using DataLayer.SSOT;
+using DNTPersianUtils.Core;
 namespace Elevator.Controllers
 {
     public class ProductController : BaseController
     {
-        private readonly ProductRepostitory productRepostitory;
+        private readonly ProductRepostitory _productRepository;
+        private readonly ProductDiscountRepository _productDiscountRepository;
 
-        public ProductController(ProductRepostitory productRepostitory)
+        public ProductController(ProductRepostitory productRepostitory,ProductDiscountRepository productDiscountRepository)
         {
-            this.productRepostitory = productRepostitory;
+            _productRepository = productRepostitory;
+            _productDiscountRepository = productDiscountRepository;
         }
 
 
@@ -26,20 +30,23 @@ namespace Elevator.Controllers
             return View();
         }
 
-
         /// <summary>
-        /// نمایش اخرین محصولات ثبت شده
+        /// محاسبه تخفیف محصول
         /// </summary>
         /// <returns></returns>
-        public async Task<IActionResult> ShowLastProduct()
+        public async Task<IActionResult> CalculateDiscount(int id)
         {
-            var model = await productRepostitory.TableNoTracking
-                .Where(a => a.IsActive == true)
-                .OrderByDescending(a => a.CreateDate)
-                .Take(7)
-                .ToListAsync();
+            var productDiscount = await _productDiscountRepository.GetByConditionAsync(a => a.ProductId == id);
+            if (productDiscount == null) return Json(false);
 
-            return PartialView(model);
+            var product = await _productRepository.GetByIdAsync(id);
+
+            var calculate = productDiscount.DiscountType == ProductDiscountSSOT.Percent ?
+                ((product.Price * productDiscount.Discount) / 100)
+                : (product.Price - productDiscount.Discount);
+
+            return Json(new Tuple<string, string, int>(calculate.ToString("n0").ToPersianNumbers(), productDiscount.Discount.ToString("n0").ToPersianNumbers(), (int)productDiscount.DiscountType));
+
         }
     }
 }
---
+++ Elevator/wwwroot/js/ProductDiscount.js
@@ -0,0 +1,19 @@
+﻿
+
+$(function () {
+    var productBoxes = $('.discountProduct');
+    $.each(productBoxes, function (index, key) {
+        var id = $(key).attr('data-id');
+        $.get("/Product/CalculateDiscount", { id: id }, function (result) {
+            if (result !== false) {
+                console.log(result);
+                $(key).append('<li class="flag-discount">' + result.item2 + "  " + (result.item3 == 1 ? "%" : "تومان") + '</li>');
+                $('.dicountValue_' + id).addClass("discount");
+                $('.dicountValue_' + id).html(result.item2 + "  " + (result.item3 == 1 ? "%" : "تومان"));
+                $('.DiscountPrice_' + id).html(result.item1 + " تومان");
+            }
+        })
+
+    })
+
+})
