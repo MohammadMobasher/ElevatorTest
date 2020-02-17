154ac8942b72e6cbe5ae7e1ee25d4e081489ec15
EmraanJadidi <emraanjadidi2@gmail.com>
2020-02-17 13:22:39 +0330

    Develop Of Discount

---
+++ Elevator/ViewComponents/CountDownProductVC.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Service.Repos;
+using Service.Repos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elevator.ViewComponents
+{
+    public class CountDownProductVC : ViewComponent
+    {
+
+        private readonly ProductDiscountRepository _productDiscountRepository;
+
+        public CountDownProductVC(ProductDiscountRepository productDiscountRepository)
+        {
+            _productDiscountRepository = productDiscountRepository;
+        }
+
+
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var model = _productDiscountRepository.TableNoTracking.FirstOrDefault(a => a.ProductId == id);
+            return View(model);
+        }
+
+    }
+}
--- Elevator/wwwroot/js/ProductDiscount.js
+++ Elevator/wwwroot/js/ProductDiscount.js
@@ -11,7 +11,7 @@ $(function () {
                 $('.dicountValue_' + id).addClass("discount");
                 $('.dicountValue_' + id).html(result.item2 + "  " + (result.item3 == 1 ? "%" : "تومان"));
                 $('.DiscountPrice_' + id).html(result.item1 + " تومان");
-                $('.countdown_Id_' + id).attr("data-countdown",result.item3);
+                $('.countdown_Id_' + id).html('<div class="hoproduct-countdown countdown" data-countdown="'+result.item4+'"></div>');
             }
         })
 
