af9b0f5fa508ab3a57afce745d8aeb7bcf642906
EmraanJadidi <emraanjadidi2@gmail.com>
2020-02-17 14:46:52 +0330

    Develop OF SomeThing

---
+++ Elevator/ViewComponents/ProductCategoryVC.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper.QueryableExtensions;
+using DataLayer.DTO.Products;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Service.Repos;
+using Service.Repos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elevator.ViewComponents
+{
+    public class ProductCategoryVC : ViewComponent
+    {
+
+        private readonly ProductRepostitory _productRepostitory;
+        private readonly ProductGroupRepository _productGroupRepository;
+
+        public ProductCategoryVC(ProductRepostitory productRepostitory
+            , ProductGroupRepository productGroupRepository)
+        {
+            _productRepostitory = productRepostitory;
+            _productGroupRepository = productGroupRepository;
+        }
+
+
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var LastProduct =await  _productRepostitory.TableNoTracking
+                .Where(a => a.IsActive == true)
+                .ProjectTo<ProductFullDTO>()
+                .OrderByDescending(a => a.CreateDate)
+                .ToListAsync();
+
+            ViewBag.Group = await _productGroupRepository.TableNoTracking.ToListAsync();
+
+            return View(LastProduct);
+        }
+    }
+}
