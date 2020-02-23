5104750806fc7a7399dc35872b991db9d4dcfb04
EmraanJadidi <emraanjadidi2@gmail.com>
2020-02-23 14:31:43 +0330

    Develop Of Package Ui

--- Elevator/ViewComponents/RelatedProductVC.cs
+++ Elevator/ViewComponents/RelatedProductVC.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper.QueryableExtensions;
 using DataLayer.DTO.Products;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Service.Repos;
 using System;
 using System.Collections.Generic;
@@ -12,18 +13,33 @@ namespace Elevator.ViewComponents
     public class RelatedProductVC : ViewComponent
     {
         private readonly ProductRepostitory _productRepostitory;
+        private readonly ProductPackageDetailsRepostitory _productPackageDetailsRepostitory;
 
-        public RelatedProductVC(ProductRepostitory productRepostitory)
+        public RelatedProductVC(ProductRepostitory productRepostitory,
+            ProductPackageDetailsRepostitory productPackageDetailsRepostitory)
         {
             _productRepostitory = productRepostitory;
+            _productPackageDetailsRepostitory = productPackageDetailsRepostitory;
         }
 
-        public IViewComponentResult Invoke(int? groupId)
+        public IViewComponentResult Invoke(int? groupId, int? packageId)
         {
-            var model = _productRepostitory.TableNoTracking.Where(a => groupId !=null&& a.ProductGroupId == groupId
-            && a.IsActive == true).OrderByDescending(a => a.CreateDate).Take(12).ToList();
+            if (packageId != null)
+            {
+                var model = _productPackageDetailsRepostitory.TableNoTracking
+                    .Include(a => a.Product)
+                    .Take(12)
+                    .Select(a => a.Product).ToList();
 
-            return View(model);
+                return View(model);
+            }
+            else
+            {
+                var model = _productRepostitory.TableNoTracking.Where(a => groupId != null && a.ProductGroupId == groupId
+                && a.IsActive == true).OrderByDescending(a => a.CreateDate).Take(12).ToList();
+
+                return View(model);
+            }
         }
     }
 }
