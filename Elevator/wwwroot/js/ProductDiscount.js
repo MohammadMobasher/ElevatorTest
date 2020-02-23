4932fcaf7b10586e92bf58ec1c903f438acaa06f
EmraanJadidi <emraanjadidi2@gmail.com>
2020-02-23 16:04:54 +0330

    Modify

--- Elevator/wwwroot/js/ProductDiscount.js
+++ Elevator/wwwroot/js/ProductDiscount.js
@@ -9,10 +9,10 @@ $(function () {
             if (result !== false) {
                 debugger;
                 console.log(result);
-                $(key).append('<li class="flag-discount">' + result.item2 + "  " + (result.item3 == 1 ? "% تخفیف" : "تومان تخفیف") + '</li>');
+                $(key).append('<li class="flag-discount">' + result.item2 + "  " + (result.item3 == 1 ? "% تخفیف" : "تومان ") + '</li>');
                 $('.dicountValue_' + id).addClass("discount");
-                $('.dicountValue_' + id).html(result.item2 + "  " + (result.item3 == 1 ? "%" : "تومان تخفیف"));
-                $('.DiscountPrice_' + id).html(result.item1 + " تومان تخفیف");
+                $('.dicountValue_' + id).html(result.item2 + "  " + (result.item3 == 1 ? "% تخفیف" : "تومان "));
+                $('.DiscountPrice_' + id).html(result.item1 + " تومان ");
                 $('.countdown_Id_' + id).html('<div class="hoproduct-countdown countdown" data-countdown="' + result.item4 + '"></div>');
             }
             else {
