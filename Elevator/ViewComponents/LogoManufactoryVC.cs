b7880b71ffa68e1e2d14e0a514440b301f2c855a
mohammad mobasher <m.mobasher.z@gmail.com>
2020-03-07 14:42:00 +0330

    use database in LogosManufactory Site

---
+++ Elevator/ViewComponents/LogoManufactoryVC.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Service.Repos;
+using Service.Repos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elevator.ViewComponents
+{
+    public class LogoManufactoryVC : ViewComponent
+    {
+
+        private readonly LogoManufactoryRepository _logoManufactoryRepository;
+
+        public LogoManufactoryVC(LogoManufactoryRepository logoManufactoryRepository)
+        {
+            _logoManufactoryRepository = logoManufactoryRepository;
+        }
+
+
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var model = _logoManufactoryRepository.TableNoTracking.ToList();
+            return View(model);
+        }
+
+    }
+}
