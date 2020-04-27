b955ae1aab36c2b0503596960ed2702a3fa66edf
Emraanjadidi <emraan.jadidi@outlook.com>
2020-04-27 21:59:27 +0430

    Develop SomeThing

---
+++ DataLayer/DTO/ProductPackage/PackageDiscountDTO.cs
@@ -0,0 +1,29 @@
+﻿using DataLayer.SSOT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.DTO.ProductDiscounts
+{
+   public class PackageDiscountDTO
+    {
+        public int Id { get; set; }
+        public long Discount { get; set; }
+
+        public int? PackageId { get; set; }
+
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public ProductDiscountSSOT DiscountType { get; set; }
+
+        public bool IsArchive { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+        public DateTime? CreateDate { get; set; }
+    }
+}
