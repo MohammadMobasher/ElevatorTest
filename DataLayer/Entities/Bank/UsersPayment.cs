3646c5837fc3efcf05af9e5b90b015ae4c3615f8
Emraanjadidi <emraan.jadidi@outlook.com>
2020-05-11 21:35:52 +0430

    Add UserPayment Enitity

---
+++ DataLayer/Entities/Bank/UsersPayment.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities.Bank
+{
+    public class UsersPayment : BaseEntity
+    {
+        public int UserId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public long Amount { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public string Token { get; set; }
+
+
+        public virtual Users.Users Users { get; set; }
+    }
+}
---
+++ Service/Migrations/20200511163947_UsersPayment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Migrations
+{
+    public partial class UsersPayment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersPayment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<long>(nullable: false),
+                    MyProperty = table.Column<int>(nullable: false),
+                    Token = table.Column<string>(nullable: true),
+                    UsersId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersPayment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersPayment_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersPayment_UsersId",
+                table: "UsersPayment",
+                column: "UsersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersPayment");
+        }
+    }
+}
