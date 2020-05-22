d8339092d4f478f5227f718f86bc71cbdb7052dc
mohammad mobasher <m.mobasher.z@gmail.com>
2020-05-22 16:23:23 +0430

    fix bug in search for main page

--- Service/Repos/Product/ProductRepostitory.cs
+++ Service/Repos/Product/ProductRepostitory.cs
@@ -434,23 +434,56 @@ namespace Service.Repos
             var model = TableNoTracking
                .Include(a => a.ProductGroup)
                .Where(a => a.IsActive == true && !a.IsDeleted);
+            if (vm.Group != null && vm.Group != -1)
+            {
+                string sql = @"
+                        declare @T table(Id int);
 
-            var result = model
+                        with A as (
+                           select Id, ParentId
+                               from ProductGroup
+                               where Id = " + vm.Group.Value + @"
+                               union all
+                           select c.Id, c.ParentId
+                               from ProductGroup c
+                                   join A p on p.Id = c.ParentId) 
+
+                        select Id from A";
+
+                var result = await _connection.QueryMultipleAsync(sql);
+                var groupsId = await result.ReadAsync<int>();
+
+
+                model = model
                 .WhereIf(!string.IsNullOrEmpty(vm.Title), a => a.Title.Contains(vm.Title)
                 || a.ShortDescription.Contains(vm.Title)
                 || a.Text.Contains(vm.Title)
                 || a.Tags.Contains(vm.Title))
-                .WhereIf(vm.Group != null && vm.Group != -1, a => a.ProductGroupId.Equals(vm.Group.Value))
+                //.WhereIf(vm.Group != null && vm.Group != -1, a => a.ProductGroupId.Equals(vm.Group.Value))
+                .WhereIf(vm.Group != null && vm.Group != -1, a => groupsId.Contains(a.ProductGroupId))
                 .WhereIf(vm.MaxPrice != null && vm.MinPrice != null, a => a.Price >= long.Parse(vm.MinPrice) && a.Price <= long.Parse(vm.MaxPrice));
+
+            }
+
+            else
+            {
+                model = model
+                    .WhereIf(!string.IsNullOrEmpty(vm.Title), a => a.Title.Contains(vm.Title)
+                    || a.ShortDescription.Contains(vm.Title)
+                    || a.Text.Contains(vm.Title)
+                    || a.Tags.Contains(vm.Title))
+                    
+                    .WhereIf(vm.MaxPrice != null && vm.MinPrice != null, a => a.Price >= long.Parse(vm.MinPrice) && a.Price <= long.Parse(vm.MaxPrice));
+            }
 
 
             if (skip != 0)
-                result = result.Skip((skip - 1) * take);
+                model = model.Skip((skip - 1) * take);
 
             if (take != 0)
-                result = result.Take(take);
+                model = model.Take(take);
 
-            return await result.ToListAsync();
+            return await model.ToListAsync();
         }
 
         /// <summary>
