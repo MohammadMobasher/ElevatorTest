3ac2931988479f8faab79a81bcc81f029a9137f2
Emraanjadidi <emraan.jadidi@outlook.com>
2020-05-22 17:55:27 +0430

    DEvelop OF User Payment

--- ElevatorNewUI/Controllers/Product/UserOrderController.cs
+++ ElevatorNewUI/Controllers/Product/UserOrderController.cs
@@ -6,6 +6,7 @@ using Elevator.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Service.Repos;
+using Service.Repos.User;
 
 namespace ElevatorNewUI.Controllers.Product
 {
@@ -13,10 +14,19 @@ namespace ElevatorNewUI.Controllers.Product
     public class UserOrderController : BaseController
     {
         private readonly ShopOrderRepository _shopOrderRepository;
+        private readonly ShopProductRepository _shopProductRepository;
+        private readonly UserRepository _userRepository;
+        private readonly UserAddressRepository _userAddressRepository;
 
-        public UserOrderController(ShopOrderRepository shopOrderRepository)
+        public UserOrderController(ShopOrderRepository shopOrderRepository
+            , ShopProductRepository shopProductRepository
+            , UserRepository userRepository
+            , UserAddressRepository userAddressRepository)
         {
             _shopOrderRepository = shopOrderRepository;
+            _shopProductRepository = shopProductRepository;
+            _userRepository = userRepository;
+            _userAddressRepository = userAddressRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -24,6 +34,19 @@ namespace ElevatorNewUI.Controllers.Product
             var model = await _shopOrderRepository
                 .GetListAsync(a => a.UserId == UserId && a.IsSuccessed);
 
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Result(string orderId,int shopOrderId)
+        {
+            var userInfo = await _userRepository.GetByConditionAsync(a => a.Id == UserId);
+            var model = await _shopOrderRepository.GetByConditionAsync(a => a.OrderId == orderId && a.UserId == UserId);
+            ViewBag.Order = await _shopProductRepository.GetListAsync(a => a.ShopOrderId == shopOrderId && a.UserId == UserId,null, "Product,ProductPackage");
+
+            ViewBag.PhoneNumber = userInfo.PhoneNumber;
+            ViewBag.FullName = userInfo.FirstName + " " + userInfo.LastName;
+            ViewBag.UserAddress = await _userAddressRepository.GetByConditionAsync(a => a.UserId == UserId);
 
             return View(model);
         }
