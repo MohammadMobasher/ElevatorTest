cfed12d377e6a9141cfe8a0f6789eed20fe1d46c
Emraanjadidi <emraan.jadidi@outlook.com>
2020-05-23 18:56:27 +0430

    Develop OF NEw Product

---
+++ ElevatorNewUI/ViewComponents/NewProductVC.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Repos;
+using Service.Repos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevatorNewUI.ViewComponents
+{
+    public class NewProductVC : ViewComponent
+    {
+
+        private readonly ProductRepostitory _productRepostitory;
+
+        public NewProductVC(ProductRepostitory productRepostitory)
+        {
+            _productRepostitory = productRepostitory;
+        }
+
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_productRepostitory.GetList(null, o => o.OrderByDescending(a => a.CreateDate), "", 7));
+        }
+
+    }
+}
