e5a55ff73d93923c68d7f6565f265e807679fe5f
mohammad mobasher <m.mobasher.z@gmail.com>
2020-07-18 00:11:02 +0430

    check all record is success in shoporderpayment, when change status for a factor call

--- Service/Repos/Product/ShopOrderPaymentRepository.cs
+++ Service/Repos/Product/ShopOrderPaymentRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +26,22 @@ namespace Service.Repos.Product
         //    var count = model.
 
         //}
+
+
+        /// <summary>
+        /// در این تابع مشخص می شود که آیا برای یک فاکتور تمام رکورد ها پرداخت شده است یا نه
+        /// </summary>
+        /// <param name="shopOrderId">شماره فاکتور</param>
+        /// <returns></returns>
+        public async Task<bool> AllIsPay(int shopOrderId)
+        {
+            var items = await DbContext.ShopOrderPayment.Where(x => x.ShopOrderId == shopOrderId && x.IsSuccess == false).ToListAsync();
+            if (items != null && items.Count > 0)
+                return true;
+
+            return false;
+
+        }
 
     }
 }
