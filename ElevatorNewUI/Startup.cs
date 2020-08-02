ab560ad05568f7b5d30476421cd553d3d41ac268
mohammad mobasher <m.mobasher.z@gmail.com>
2020-08-02 11:30:14 +0430

    increase login time

--- ElevatorNewUI/Startup.cs
+++ ElevatorNewUI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -48,7 +49,13 @@ namespace ElevatorNewUI
             services.DatabaseConfiguration(Configuration);
             services.AddCustomIdentity(_siteSetting.IdentitySettings);
 
-            services.AddAuthentication();
+            services.Configure<SecurityStampValidatorOptions>(options => options.ValidationInterval = TimeSpan.FromSeconds(10));
+
+            services.AddAuthentication().Services.ConfigureApplicationCookie(options =>
+            {
+                options.SlidingExpiration = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            });
 
             #region Mapper
             services.AddAutoMapper(typeof(SlideShowMapper));
