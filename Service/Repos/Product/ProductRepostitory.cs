88b265b6f6b57c4c0cd06bf0d06da5d68fdf0c46
mohammad mobasher <m.mobasher.z@gmail.com>
2020-08-16 10:36:28 +0430

    use IsDeleted in GetProductQuery function

--- Service/Repos/Product/ProductRepostitory.cs
+++ Service/Repos/Product/ProductRepostitory.cs
@@ -402,7 +402,7 @@ namespace Service.Repos
                         (featureSearchableVM != null && featureSearchableVM.Count > 0 ? featureSearchWhere : " where ")
                     + @"
                     ProductGroupId in (" + ((selectedsubGroup != null && selectedsubGroup.Count > 0 ? groupAndSubGroupId : "select * from @T")) + @")
-                        and Title LIKE '%" + titleSearch + @"%';";
+                        and Title LIKE '%" + titleSearch + @"%' and Product.IsDeleted = 0;";
 
             var productQuery = $@"                    
                     select Product.* from Product " +
