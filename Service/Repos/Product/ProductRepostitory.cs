e040a52bf3071f998c1e9d3fe86d67e9186bfd80
mohammad mobasher <m.mobasher.z@gmail.com>
2020-08-21 17:06:39 +0430

    fix some bug

--- Service/Repos/Product/ProductRepostitory.cs
+++ Service/Repos/Product/ProductRepostitory.cs
@@ -504,12 +504,12 @@ namespace Service.Repos
 
                 model = model
                 .WhereIf(!string.IsNullOrEmpty(vm.Title), a =>
-                a.Title.CleanString().Contains(vm.Title.CleanString())
-                || a.ShortDescription.CleanString().Contains(vm.Title.CleanString())
+                a.Title.Contains(vm.Title.CleanString())
+                || a.ShortDescription.Contains(vm.Title.CleanString())
                 //|| a.Text.CleanString().Contains(vm.Title.CleanString())
                 //|| a.Text.Contains(vm.Title)
-                || a.SearchKeyWord.CleanString().Contains(vm.Title.CleanString())
-                || a.Tags.CleanString().Contains(vm.Title.CleanString()))
+                || a.SearchKeyWord.Contains(vm.Title.CleanString())
+                || a.Tags.Contains(vm.Title.CleanString()))
                 //.WhereIf(vm.Group != null && vm.Group != -1, a => a.ProductGroupId.Equals(vm.Group.Value))
                 .WhereIf(vm.Group != null && vm.Group != -1, a => groupsId.Contains(a.ProductGroupId))
                 .WhereIf(vm.MaxPrice != null && vm.MinPrice != null, a => a.Price >= long.Parse(vm.MinPrice) && a.Price <= long.Parse(vm.MaxPrice));
