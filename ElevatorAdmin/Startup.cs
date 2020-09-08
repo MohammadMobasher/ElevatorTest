bde4f84e242f8e879fc2b17ddb5f3e16e234ab8d
mohammad mobasher <m.mobasher.z@gmail.com>
2020-09-08 18:03:28 +0430

    change startup

--- ElevatorAdmin/Startup.cs
+++ ElevatorAdmin/Startup.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using Core;
 using ElevatorAdmin.Hubs;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json.Serialization;
 using Service;
 using WebFramework.Configurations;
 
@@ -21,7 +26,7 @@ namespace ElevatorAdmin
 {
     public class Startup
     {
-        
+
         private readonly SiteSettings _siteSetting;
         public IConfiguration Configuration { get; }
 
@@ -32,7 +37,7 @@ namespace ElevatorAdmin
             _siteSetting = configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>();
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -43,14 +48,64 @@ namespace ElevatorAdmin
             //    .AddJsonFile($"appsettings.json")
             //    .Build());
 
-            services.AddHttpClient();
+            services.AddSingleton<HtmlEncoder>(
+                HtmlEncoder.Create(allowedRanges: new[]
+                {
+                    UnicodeRanges.BasicLatin,
+                    UnicodeRanges.Arabic
+                }));
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                config.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .AddJsonOptions(x =>
+                {
+                    x.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+
+            services.AddDistributedMemoryCache();
+            services.AddResponseCaching();
+
+            services.AddSession(options =>
+            {
+                options.CookieName = ".elevator.Session";
+                options.IdleTimeout = TimeSpan.FromSeconds(20);
+                options.CookieHttpOnly = true;
+            });
+
+
+            //services.AddHttpClient();
 
 
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = ".elevator.admin";
+                options.Cookie.Expiration = DateTime.Now.Subtract(DateTime.UtcNow).Add(TimeSpan.FromHours(5));
+
+                options.ExpireTimeSpan = DateTime.Now.Subtract(DateTime.UtcNow).Add(TimeSpan.FromHours(5));
+
+                options.SlidingExpiration = true;
+            });
+
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = DateTime.Now.Subtract(DateTime.UtcNow).Add(TimeSpan.FromHours(5));
+                //options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
             });
 
 
@@ -63,28 +118,24 @@ namespace ElevatorAdmin
 
             services.AddDistributedMemoryCache();
 
-         
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddSessionStateTempDataProvider();
-
-            services.AddSession(options =>
-            {
-                // Set a short timeout for easy testing.
-                options.IdleTimeout = TimeSpan.FromMinutes(1000000);
-                //options.Cookie.HttpOnly = true;
-                // Make the session cookie essential
-                options.Cookie.IsEssential = true;
-            });
 
 
-            services.Configure<SecurityStampValidatorOptions>(options => options.ValidationInterval = TimeSpan.FromMinutes(1000000));
+            services.AddMvc();
+            //.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+            //    .AddSessionStateTempDataProvider();
 
-            services.AddAuthentication().Services.ConfigureApplicationCookie(options =>
-            {
-                options.SlidingExpiration = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(1000000);
-            });
+
+
+
+
+            //services.Configure<SecurityStampValidatorOptions>(options =>
+            //{
+            //    options.ValidationInterval = TimeSpan.FromMinutes(1000000)
+            //});
+
+            
+
+
 
         }
 
@@ -94,7 +145,8 @@ namespace ElevatorAdmin
 
             //if (env.IsDevelopment())
             //{
-                app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
+            
             //}
             //else
             //{
@@ -103,7 +155,7 @@ namespace ElevatorAdmin
 
             // Redirect StatusCode 400 & 404
             app.UseRedirectConfigure();
-
+            app.UseCookiePolicy();
             app.UseStaticFiles();
             app.UseAuthentication();
 
@@ -113,9 +165,9 @@ namespace ElevatorAdmin
             //    route.MapHub<UserOnlineCountHub>("/");
             //});
 
+            app.UseSession(new SessionOptions() { Cookie = new CookieBuilder() { Name = ".AspNetCore.Session.ElevatorAdmin" } });
 
-
-            app.UseSession();
+            //app.UseSession();
 
             app.UseMvc(routes =>
             {
--- WebFramework/Configurations/IdentityConfigureExtensions.cs
+++ WebFramework/Configurations/IdentityConfigureExtensions.cs
@@ -29,9 +29,10 @@ namespace WebFramework.Configurations
                 //identityOptions.SignIn.RequireConfirmedEmail = false;
                 //identityOptions.SignIn.RequireConfirmedPhoneNumber = false;
 
+
                 //Lockout Settings
-                identityOptions.Lockout.MaxFailedAccessAttempts = 5;
-                identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                identityOptions.Lockout.MaxFailedAccessAttempts = 10;
+                identityOptions.Lockout.DefaultLockoutTimeSpan = DateTime.Now.Subtract(DateTime.UtcNow).Add(TimeSpan.FromHours(5));
                 identityOptions.Lockout.AllowedForNewUsers = false;
             })
             .AddEntityFrameworkStores<DatabaseContext>();
